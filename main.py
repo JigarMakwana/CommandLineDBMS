d7e2cdadaef633e6ef72e77bc21264deac244194
Karan Kharecha <karan.kharecha@dal.ca>
2020-11-25 00:34:13 -0400

    query execution method created with file as table and directory as database

--- main.py
+++ main.py
@@ -4,20 +4,14 @@ from queryExecutor.queryExecutor import executeQuery
 is_logged_in = True
 isin_session = True
 
-
-def start_db():
-    # while True:
-    parser = parseQuery('select * from employee where s = 2')
-    executeQuery(parser)
-
-
 if __name__ == '__main__':
     while isin_session:
-        username = input("Enter username:")
-        password = input("Enter password:")
+        username = input("Enter username: ")
+        password = input("Enter password: ")
         print("Signing in...")
         # invoke sign in method
         # is_logged_in = sign_in(username, password)
         if is_logged_in:
-            start_db()
+            dbname = input("Enter database: ")
+            executeQuery(dbname + "/")
             isin_session = False
--- queryExecutor/queryExecutor.py
+++ queryExecutor/queryExecutor.py
@@ -1,2 +1,28 @@
-def executeQuery(parser):
-    print(parser)
+from os import path, listdir
+import queryParser.queryParser as qp
+
+db_path = "database/"
+
+
+def executeQuery(dbname):
+    db = db_path + dbname
+    if path.exists(db):
+
+        # database: validation
+        print("Into the database: ", dbname)
+        qp.tableNames = listdir(db_path + dbname)
+
+        # query: validation and parsing
+        while True:
+            query = input(">> ")
+            try:
+                parser = qp.parseQuery(query)
+
+                # query: execution
+                with open(db + parser['Table'][0].lower()) as table:
+                    print(table.read())
+            except:
+                print("Operation failed, try again..")
+
+    else:
+        print("Invalid database name")
